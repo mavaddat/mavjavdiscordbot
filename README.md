1b7b31d6844afbb7b74e05121bf627554a4ed262
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-03-28 17:38:52 -0700

    authToken class created

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mavjavdiscordbot
+# Mav Jav Discordbot
+
 [Discord4J](https://github.com/Discord4J) bot for [Discord](https://discordapp.com/developers/docs/intro), all-in-one voice and text chat for gamers
+
+```bash
+mvn archetype:generate -DgroupId=com.discord4j.discord4j -DartifactId=discord4j-core -DarchetypeVersion=3.1.0.TEST-SNAPSHOT
+```
--- discord4j-core/src/main/java/com/discord4j/discord4j/App.java
+++ discord4j-core/src/main/java/com/discord4j/discord4j/App.java
@@ -7,21 +7,37 @@ import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
+import org.identityconnectors.common.security.GuardedString;
+import com.discord4j.discord4j.AuthToken;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        final DiscordClient client = DiscordClientBuilder.create("SUPER-SECRET-AUTH-KEY").build();
-        //Attempting to follow https://github.com/Discord4J/Discord4J/wiki/Migrating-from-v3.0-to-v3.1
+public class App {
+    public static void main(String[] args) {
+        //Using guide from https://www.java2depth.com/2018/05/secure-sensitive-data-while-storing-and.html
+        GuardedString authToken = AuthToken.getAuthToken(); //getAuthToken() is a static method of our custom AuthToken class that returns the auth token as a GuardedString object
+        final StringBuilder clearAuthToken = new StringBuilder();
+        authToken.access(new GuardedString.Accessor() {
+            @Override
+            public void access(final char[] clearChars) {
+                clearAuthToken.append(clearChars);
+            }
+        });
+        final DiscordClient client = DiscordClientBuilder.create(clearAuthToken.toString()).build();
+        authToken.dispose();
+        // Attempting to follow
+        // https://github.com/Discord4J/Discord4J/wiki/Migrating-from-v3.0-to-v3.1
         GatewayDiscordClient gateway = client.login().block();
-        gateway.on(ReadyEvent.class).subscribe(ready -> System.out.println("Logged in as " +ready.getSelf().getUsername()));
-        gateway.on(MessageCreateEvent.class, event -> Mono.just(event.getMessage()).filter(message -> message.getContent().map("!ping"::equals).orElse(false)).flatMap(Message::getChannel).flatMap(channel -> channel.createMessage("Pong!"))).subscribe();
+        gateway.on(ReadyEvent.class)
+                .subscribe(ready -> System.out.println("Logged in as " + ready.getSelf().getUsername()));
+        gateway.on(MessageCreateEvent.class,
+                event -> Mono.just(event.getMessage())
+                        .filter(message -> message.getContent().map("!ping"::equals).orElse(false))
+                        .flatMap(Message::getChannel).flatMap(channel -> channel.createMessage("Pong!")))
+                .subscribe();
         gateway.onDisconnect().block();
 
-    }   
+    }
 }
